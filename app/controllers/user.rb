5d177f12ef253e21455db157684e289d4f6dd8ad
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 15:34:50 -0500

    created session

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -8,8 +8,11 @@ post '/gamehome' do
   if params["password_confirmation"]
     if params["password_confirmation"] == params[:new_user]["password"]
       @user = User.create(params[:new_user])
-      erb :new_game
       #sessions
+      if @user != nil
+        session[:user_id] = @user.id
+      end
+      erb :gamehome
     else
       #invalid signup
       @error = "Passwords Don't Match"
@@ -17,6 +20,13 @@ post '/gamehome' do
     end
   else
     #Sign In Case
-    puts false
+    puts @user = User.where(:username => params["username"], :password => params["password"]).first
+    if !@user.nil?
+      erb :gamehome
+    else
+      #invalid signin
+      @error = "Invalid Username or Password"
+      erb :signin
+    end
   end
 end
