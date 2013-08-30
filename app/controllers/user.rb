3f3d2b871db931e57dd8093c847f58ec632f8209
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 14:57:38 -0500

    updated sign up

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -8,6 +8,13 @@ post '/gamehome' do
   #Sign Up Case
   if params["password_confirmation"]
     puts true
+    if params["password_confirmation"] == params["password"]
+      #Create User
+    else
+      #invalid signup
+      @error = "Passwords Don't Match"
+      erb :signup
+    end
   else
     #Sign In Case
     puts false
