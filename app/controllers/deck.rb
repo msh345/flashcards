fc7500d9059f1a2876ac8629a1ea152007a1dc7a
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-30 16:26:13 -0500

    .

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,7 +1,14 @@
 # GET ====================
 get '/deck/:id' do
   deck_clone = Deck.find(params[:id])
-  puts deck_clone.cards.inspect
-  @cards
-  erb :game_start 
+   @cards = deck_clone.cards
+   @cards.each do |c|
+    @c = c
+    puts c.term
+    erb :game_start
+  end
+  # @cards.each do |card|
+  #   erb :game_start
+  # end
+  
 end
